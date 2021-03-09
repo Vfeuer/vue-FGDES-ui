69a5ccfcda22ecf1013b45c7c1efd36917dbf163
Vfeuer <xinhuang.gong@fau.de>
2021-03-09 02:17:42 +0100

    delete useless

--- src/main.js
+++ src/main.js
@@ -11,11 +11,7 @@ import 'nprogress/nprogress.css'
 
 locale.use(lang)
 
-<<<<<<< HEAD
 axios.defaults.baseURL = 'http://localhost:3000/api/'
-=======
-axios.defaults.baseURL = 'http://127.0.0.1:3000/api/'
->>>>>>> af80316ae2929344f73cfe210e261d4f858a6568
 
 // get token, get permission for data
 axios.interceptors.request.use(config => {
